923c9effb78045c62acf317b367680f90afeeb17
yanqi.zong <yzong@turo.com>
2025-04-12 17:13:13 -0700

    feat: update release workflow

--- release.config.cjs
+++ release.config.cjs
@@ -1,19 +1,3 @@
 module.exports = {
-  branches: [
-    "main",
-    {
-      name: "f/*",
-      channel: "next",
-      prerelease:
-        "beta-${(/^[a-zA-Z]+-[0-9]+/.exec(name.substr(2)) || [name.replace(/[_/.]/g, '-')])[0]}-${Date.now()}",
-    },
-  ],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
-    "@semantic-release/github",
-    "@semantic-release/git",
-    "@semantic-release/changelog",
-  ],
+  branches: ["main"],
 };
